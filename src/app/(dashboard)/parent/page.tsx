a6412637e180a627d8415261528ef8d16872bbbc
TimNet-codx <jesulayomitimze55@gmail.com>
2026-02-17 08:50:47 +0100

    Update V2

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,7 +1,22 @@
+import Announcements from "@/src/components/Announcements";
+import BigCalendar from "@/src/components/BigCalender";
+import EventCalendar from "@/src/components/EventCalendar";
+
 const ParentPage = () => {
-    return(
-        <div className="">
-           <h1>Parent Page</h1>
+    return (
+        <div className="p-4 flex gap-4 flex-col xl:flex-row">
+            {/* LEFT */}
+            <div className="w-full xl:w-2/3">
+             <div className="h-full bg-white p-4 rounded-md">
+                <h1 className="text-xl font-semibold">Schedule (Ayo Mark)</h1>
+                  <BigCalendar /> 
+             </div>
+            </div>
+            {/* RIGHT */}
+            <div className="w-full xl:w-1/3 flex flex-col gap-8">
+            <EventCalendar />
+            <Announcements />
+            </div>
         </div>
     )
 }
---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const TeacherForm = ({type, data}:{type: "create" | "update"; data?: any}) => {
+  return (
+    <form>
+        
+    </form>
+  )
+}
+
+export default TeacherForm;
